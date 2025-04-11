5d3cec68adb5e1a7ef144cdd1cf04f28852477ce
Kebon420 <kevindeluca2006@gmail.com>
2025-04-12 01:30:38 +0200

    Aggiunti più commenti

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ let locale = true
 //Inserisci l'ip della macchina
 let ipLocale = "192.168.1.39"
 
-
 let baseUrl
 let prefissoVariabili = ""
+
+//Se il server è locale, le variabili non avranno alcun prefisso,
+//altrimenti avranno il prefisso "parcheggioArduino" per non intasare le variabili di rabbits
 if (locale){
-    baseUrl = `http://${ipLocale}/UI/rabbits-main/`
+    baseUrl = `http://${ipLocale}/SchoolMakerDay-Parcheggio-Interfaccia-Utente/rabbits-main/`
 }else{
     baseUrl = "https://www.schoolmakerday.it/rabbits/"
     prefissoVariabili = "parcheggioArduino"
 }
 
-
+//I comandi sono inviati dall'utente tramite il panel/dashboard
+//Gli stati sono ricevuti dall'Arduino e indicano lo stato del field
 //Gli enable fanno in modo di accettare variazioni dei comandi dall'arduino
 let nomiVariabili = [
                     "ELmp",     //Enable  Lampioni
@@ -35,23 +38,28 @@ let nomiVariabili = [
                     ]
 
 
-
+// Dichiaro la funzione rabbits che manda una richiesta http GET se nessun valore è impostato
+// Altrimenti impostando un valore la funzione manda una richiesta http POST 
 async function rabbits(key, valore = null){
-    key = prefissoVariabili + key
+    
+    //Aggiunge il prefisso alla chiave
+    key = prefissoVariabili + key //Nessun prefisso se il server è locale 
 
     let url = baseUrl
-    if(valore === null){
+    if(valore === null){ //Se il valore non viene impostato si fa una richiesta GET
         url += `get.php?key=${key}`
-    }else{
+    }else{ //Altrimenti si fa una richiesta POST
         url += `set.php?key=${key}&value=${valore}`
     }
     console.log(url)
     
     let RispostaRabbits = await fetch(url); // ASPETTA la risposta
     let data = await RispostaRabbits.json(); // ASPETTA la conversione in JSON
-    return data
+    return data //Ritorna la risposta
 }
 
+
+//Funzione per invertire lo stato di una variabile
 async function rabbitsToggle(key){
     let valoreChiave = await rabbits(key)
     valoreChiave = valoreChiave["data"]["value"]
@@ -65,6 +73,7 @@ async function rabbitsToggle(key){
     await rabbits(key, !valoreChiave)
 }
 
+//Funzione per ottenere tutte le chiavi e i loro valori corrispondenti
 async function rabbitsOttieniTutteLeKey(){
     let url = baseUrl + `getkeys.php?keys=[`
     
@@ -82,16 +91,18 @@ async function rabbitsOttieniTutteLeKey(){
     return data
     }
 
+//Funzione per dichiarare tutte le chiavi,
+//questo serve all'inizio quando le variabili non sono dichiarate,
+//una volta dichiarate non serve più, è una funzione puramente di debug
 async function rabbitsDichiaraTutteLeKey(){
     nomiVariabili.forEach(element => {
         rabbits(element, false).then(data => console.log(data))
     });
 }
 
-async function test(){
-    rabbitsOttieniTutteLeKey().then(data => console.log(data))
-}
-
+//Funzione che cambia l'interfaccia utente in base alle variabili
+//attualmente cambia il colore dei bottoni ma in futuro spero di riuscire
+//a mettere delle piccole animazioni :)
 async function sistemaUI(data){
     document.querySelector(".display").setAttribute("src",`display7Segmenti/${data["data"]["posti"]["value"]}.png`);
     
@@ -133,10 +144,10 @@ async function sistemaUI(data){
 
 }
 
+//Ciclo infinito di polling di tutte le variabili
 async function loop(){
     rabbitsOttieniTutteLeKey().then(data => sistemaUI(data))
-    await new Promise(resolve => setTimeout(resolve, 100));
-    console.log("loop")
+    await new Promise(resolve => setTimeout(resolve, 100)); //Attende 100ms
     loop()
 }
 
